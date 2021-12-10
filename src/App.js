e4ccadbaaf0e2b4403e342603ea1c76ad630b4e7
Aryan Phulkar <aryanphulkar1@gmail.com>
2021-12-11 00:17:35 +0530

    Added mobile filters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "./Components/Header/Header";
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Mobile from "./Components/Mobile/Mobile";
+import Body from "./Components/Body_Home/Body";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <div>
         <Header />
       </div>
+      .+
+      <Switch>
+      <Route path="/" exact component={Body}></Route>
+      <Route path="/Mobile" exact component={Mobile}></Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
